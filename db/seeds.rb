7771e1da42ec5d73f89dc664101efe7259e8886f
sh33pnik <nomail@sh33pnik>
2023-09-04 14:02:32 +0200

    seed für artikelkategorie

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,5 @@
 #   Character.create(name: "Luke", movie: movies.first)
 
 artikel = Artikel.create([{name: "Kaffee-Niklas-Crema", id_kategorie: 1, preis: 4}])
+
+artikelkategorie = Artikelkategorie.create([{kategorie: "Snacks"}, {kategorie: "Kaltgetränke"}, {kategorie: "Heißgetränke"}])
