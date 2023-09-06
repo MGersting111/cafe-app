aa9c68b1ade1f0032f6b42159c5b9174d4335dfe
Micha Gersting <gerstingmicha@gmail.com>
2023-09-06 13:51:13 +0200

    Warenkorb hinzugefügt

--- app/controllers/artikels_controller.rb
+++ app/controllers/artikels_controller.rb
@@ -57,10 +57,6 @@ class ArtikelsController < ApplicationController
     end
   end
 
-  def add
-
-  end
-
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_artikel
