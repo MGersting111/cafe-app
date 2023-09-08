8984c0a95d99dfdf16307f4233c1a0c7fd1fa6e9
Micha Gersting <gerstingmicha@gmail.com>
2023-09-08 10:35:06 +0200

    refactored Tables

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,9 @@
 Rails.application.routes.draw do
   resources :artikelkategories do
     resources :artikels
-    get "add/:id", to:"artikels#add", as: "artikels_add"
   end
   resources :artikels, only: %i[new create]
 
   root 'artikelkategories#index'
-  resources :bestellungs
-  resources :bestellung_artikels
-  resources :warenkorbs
-
+ 
 end
