aa2f2b6fc049523bded8995f331e1567ecada5e0
Micha Gersting <gerstingmicha@gmail.com>
2023-09-08 10:28:54 +0200

    refactored Tables

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   resources :artikelkategories do
     resources :artikels
+    get "add/:id", to:"artikels#add", as: "artikels_add"
   end
   resources :artikels, only: %i[new create]
 
   root 'artikelkategories#index'
-  get "add/:id", to:"artikels#add", as: "artikels_add"
   resources :bestellungs
   resources :bestellung_artikels
   resources :warenkorbs
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,8 @@
 
-Artikel.destroy_all
-Artikelkategorie.destroy_all
-Bestellung.destroy_all
-BestellungArtikel.destroy_all
+#Artikel.destroy_all
+#Artikelkategorie.destroy_all
 
-artikel = Artikel.create([{name: "Brezel", artikelkategorie_id: 1, preis: 100}, {name: "Fanta", artikelkategorie_id: 2, preis: 150}])
+
 
 artikelkategorie = Artikelkategorie.create([{kategorie: "Snacks"}, {kategorie: "Kaltgetränke"}, {kategorie: "Heißgetränke"}])
-
-bestellung = Bestellung.create([{tischnummer: 1},{tischnummer: 2},{tischnummer: 2}])
-
-bestellung_artikel = BestellungArtikel.create([{id_bestellung: 28, id_artikel: 2, anzahl: 4}, {id_bestellung: 29, id_artikel: 3, anzahl: 2}, {id_bestellung: 30, id_artikel: 1, anzahl: 1}])
+artikel = Artikel.create([{name: "Brezel", artikelkategorie_id: 1, preis: 100}, {name: "Fanta", artikelkategorie_id: 2, preis: 150}])
