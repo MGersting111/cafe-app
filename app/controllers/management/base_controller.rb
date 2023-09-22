5b82cf1414ee014fbf2df963a417aefafe737c74
Andreas Schwarzkopf <asmailbox@gmx.de>
2023-09-22 16:05:42 +0200

    Make ManagementBase controller redirect to root page if no user is logged in

--- app/controllers/management/base_controller.rb
+++ app/controllers/management/base_controller.rb
@@ -2,5 +2,14 @@
 
 module Management
   class BaseController < ::ApplicationController
+    before_action :check_login
+
+    private
+
+    def check_login
+      return if current_user.present?
+
+      redirect_to new_user_session_path, alert: 'You need to be logged in to access this page'
+    end
   end
 end
